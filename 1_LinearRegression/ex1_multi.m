749dfb2c0166c48f9288dd2bd13d2d9101969cd6
unknown <azeez@labview1.ybad.ad.yaskawa.com>
2014-09-26 21:01:40 -0700

    Linear regression, optional excercises

--- 1_LinearRegression/ex1_multi.m
+++ 1_LinearRegression/ex1_multi.m
@@ -82,7 +82,7 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha = 0.1;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
@@ -104,8 +104,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+test = [1650, 3];
+test_Norm = bsxfun(@rdivide, bsxfun(@minus, test, mu), sigma);
+price = [1, test_Norm] * theta;
 
 % ============================================================
 
@@ -149,7 +150,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1, 1650, 3] * theta;
 
 
 % ============================================================
--- 1_LinearRegression/featureNormalize.m
+++ 1_LinearRegression/featureNormalize.m
@@ -25,15 +25,9 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
-
-
-
-
-
-
-
-
-
+mu = mean(X, 1);
+sigma = std(X, 1, 1);
+X_norm = bsxfun(@rdivide, bsxfun(@minus, X, mu), sigma);
 % ============================================================
 
 end
